36714a11de9a9dd2df9e9240611388660f85e2f4
charles <charles@charless-MacBook-Pro.local>
2017-01-05 12:33:47 -0600

    initial commit

--- script.js
+++ script.js
@@ -33,3 +33,67 @@ is more than one character in the string. Return `val` and set the `majorKey` to
 
 14. Your answer should be a string value that equals 7. Is that what you got?
 */
+
+var majorKey=42;
+
+if( majorKey >= 53){
+  majorKey=majorKey+42;
+}else{
+  majorKey=majorKey-13;
+}
+//console.log('step 2:'+majorKey);   // 29
+var a='11';
+majorKey=majorKey+a;
+//console.log('step 3:'+majorKey);   // '2911'
+var arr=[];
+for(var i=0;i<majorKey.length;i++){
+ arr[i]=majorKey.charAt(i);
+
+}
+//console.log('step 4: '+arr);  // [2,9,1,1]
+
+arr.pop();
+arr.shift();
+//console.log('step 5: '+arr);  // [9,1]
+
+var b='';
+for(var j=arr.length;j>0;j--){
+  b=b+arr[j-1];
+}
+//console.log('step 6: '+b);  // '19'
+
+majorKey=parseInt(majorKey);
+b=parseInt(b);
+//console.log('majorKeyInt:'+majorKey+''+'bInt:'+b);
+
+majorKey=majorKey+b;
+//console.log('step 8: ' +majorKey); //291119
+
+if(majorKey<60){
+  majorKey=14;
+}
+else if (majorKey==2930) {
+  majorKey=27;
+}
+else {
+  majorKey=2;
+}
+//console.log('step 9: ' +majorKey); //2
+
+var count=10;
+while(count>0){
+  majorKey=majorKey+1;
+  count--;
+
+}
+//console.log('step 10: ' +majorKey); //12
+
+function stepEleven(val){
+  val=val.toString();
+  if(val.length>0){
+    val=val.substring(1,val.length);
+  }
+  return val;
+}
+majorKey=stepEleven(majorKey);
+console.log('step 13: ' +majorKey);
